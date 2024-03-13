bb33e8521a8bd2bd754aa60e797513a45af370af
Sumedh Bansod <162736736+Sumedhbansod@users.noreply.github.com>
2024-03-13 16:35:00 +0530

    Update q4.sh

--- q4.sh
+++ q4.sh
@@ -2,9 +2,10 @@
 #!/bin/bash
 
 echo "Enter a number: "
-read number
+read n
 
-if (( number % 5 == 0 && number % 11 == 0 )); then
+if [n % 5 == 0 ] && [ n % 11 == 0 ] 
+then
     echo "$number is divisible by both 5 and 11."
 else
     echo "$number is not divisible by both 5 and 11."
