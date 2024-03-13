24eeeb8f8bdd052dba66cc59ac251f23e56c2549
Sumedh Bansod <162736736+Sumedhbansod@users.noreply.github.com>
2024-03-13 16:11:28 +0530

    Update q4.sh

--- q4.sh
+++ q4.sh
@@ -1,13 +1,11 @@
 //	Write a Shell Script to check whether a number is divisible by 5 and 11 or not.
-read -p " Enter the number " num1
-n=`expr $num1 % 5`
-n1=`expr $num1 % 11`
-if [ n == 0 ]
-then 
-echo " $num1 is divisible by 5 "
-elif [ n1 == o ]
-then
-echo " $num1 is divisible by 11 "
-else 
-echo " $num1 is not divisible by 5 and 11 "
+#!/bin/bash
+
+echo "Enter a number: "
+read number
+
+if (( number % 5 == 0 && number % 11 == 0 )); then
+    echo "$number is divisible by both 5 and 11."
+else
+    echo "$number is not divisible by both 5 and 11."
 fi
